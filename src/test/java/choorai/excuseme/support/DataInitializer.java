759af5b0fbc25391913d06f6446acef863bab9e9
somefood <iamseokjuhong@gmail.com>
2024-03-27 12:35:04 +0900

    #41: 미사용 테스트 클래스 제거

--- src/test/java/choorai/excuseme/support/DataInitializer.java
+++ src/test/java/choorai/excuseme/support/DataInitializer.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
